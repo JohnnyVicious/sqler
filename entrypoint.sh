bb867f8fe38a3abf5ff89dc793ad704c5089cf9b
PowerShell Script <you@example.com>
2020-04-14 13:56:32 +0200

    c64f3dd2-d03c-4fa4-9ac3-e7443aec661b (20200414 13:56)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 ls
 
+FILE=./sqler
+if [ -f "$FILE" ]; then
+    echo "$FILE exist, removing folder"
+    rm -rf ./sqler
+fi
+
+git clone https://github.com/JohnnyVicious/sqler.git
+
+cp -f ./sqler/config.*.hcl /app/
+
 sqler -driver=${DRIVER} -rest=${REST} -dsn=${DSN} -config=${CONFIG} -workers=${WORKERS} -resp=${RESP}
